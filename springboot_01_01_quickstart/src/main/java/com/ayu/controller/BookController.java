84d302dfcfa7b9bd3b59abb9c47eaa47c42c81e5
AyuStatTrak <32477130@qq.com>
2023-08-05 15:15:42 +0800

    Initial commit

---
+++ springboot_01_01_quickstart/src/main/java/com/ayu/controller/BookController.java
@@ -0,0 +1,16 @@
+package com.ayu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @GetMapping
+    public String selectAll(){
+        System.out.println("springboot is running");
+        return "springboot is running";
+    }
+}
